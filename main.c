5401cb841188cb3c2b5c3ad132f6df69494285cb
glowbulb <litebulb@rektaf.com>
2017-01-30 11:06:02 -0500

    1

--- main.c
+++ main.c
@@ -20,6 +20,11 @@ int_array_t addone_int_array_f(int_array_t a) {
     return n;
 }
 
+int_array_t subone_int_array_f(int_array_t a) {
+    const int_array_t n = { .n = { a.n[0] - 1, a.n[1] - 1 } };
+    return n;
+}
+
 int_array_t swap_int_array_f(int_array_t a) {
     const int_array_t n = { .n = { a.n[1], a.n[0] } };
     return n;
@@ -42,11 +47,12 @@ int_array_t arg_int_array_f(int argc, char** argv) {
 void_int_array_f_t print_int_array = print_int_array_f;
 int_array_f_t new_int_array = new_int_array_f;
 int_array_int_array_f_t addone_int_array = addone_int_array_f;
+int_array_int_array_f_t subone_int_array = addone_int_array_f;
 
 int main(int argc, char** argv) {
 
     if(argc < 2) { return 0; }
 
-    print_int_array(addone_int_array(swap_int_array_f(arg_int_array_f(argc, argv))));
+    print_int_array(subone_int_array(swap_int_array_f(arg_int_array_f(argc, argv))));
 
 }

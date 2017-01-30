cda9e7f45ac1dea6ca834a0493cdae551f8e5e55
glowbulb <adamg@corp.baenetworks.com>
2017-01-30 08:41:49 -0500

    1

--- main.c
+++ main.c
@@ -20,24 +20,23 @@ int_array_t addone_int_array_f(int_array_t a) {
     return n;
 }
 
-int_array_t arg_int_array_f(int argc, char** argv) {
-    if(argc <= 4 && (strcmp(argv[1], "-print") == 0)){
-        const int x = atoi(argv[2]);
-        const int y = atoi(argv[3]);
-        const int_array_t n = { .n = { x,y } };
-        return n;
-    } else {
-        const int_array_t b = { .n = { 0 } };
-        return b;
-    }
+int_array_t swap_int_array_f(int_array_t a) {
+    const int_array_t n = { .n = { a.n[1], a.n[0] } };
+    return n;
 }
 
 void print_int_array_f(int_array_t a){
-    const int s = sizeof(a.n) / sizeof(int);
-    for (int i = 0; i < s; i++) {
-        printf("%d ",a.n[i]);
+    printf("\n", printf("%d %d\n", a.n[0], a.n[1]));
+}
+
+int_array_t arg_int_array_f(int argc, char** argv) {
+    if(argc <= 4 && (strcmp(argv[1], "-print") == 0)){
+        const int_array_t n = { .n = { atoi(argv[2]), atoi(argv[3]) } };
+        return n;
+    } else {
+        const int_array_t b = { .n = { 0, 0 } };
+        return b;
     }
-    printf("\n");
 }
 
 print_int_array_t print_int_array = print_int_array_f;
@@ -46,6 +45,8 @@ addone_int_array_t addone_int_array = addone_int_array_f;
 
 int main(int argc, char** argv) {
 
-    print_int_array(addone_int_array(new_int_array()));
+    if(argc < 2) { return 0; }
+
+    print_int_array(addone_int_array(swap_int_array_f(arg_int_array_f(argc, argv))));
 
 }
